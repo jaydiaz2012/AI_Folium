b63614ddfa09ba4c9eb5b3ef2e4bf3351052c0ac
jdiaz <jayremie.diaz@gmail.com>
2024-11-27 12:50:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,11 @@ import numpy as np
 import pandas as pd
 import json
 import matplotlib.pyplot as plt 
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import CSVLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.vectorstores import Chroma
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from openai.embeddings_utils import get_embedding
-import faiss
-import warnings
-from PIL import Image
 import folium
 import streamlit as st
 import warnings
 from streamlit_option_menu import option_menu
 from streamlit_extras.mention import mention
-import base64
-import random
-from datetime import datetime, timedelta
-import requests
-import json
 from streamlit_folium import st_folium
 from folium import plugins
 from folium.plugins import MarkerCluster
